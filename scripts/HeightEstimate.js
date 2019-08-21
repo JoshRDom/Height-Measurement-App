9ecb4b03262692f6e830f95ff046e72554632ac7
Cheng Syuen <chua.cheng.syuen@gmail.com>
2019-08-21 11:05:15 +0800

    Edited and improved on previous code

--- scripts/HeightEstimate.js
+++ scripts/HeightEstimate.js
@@ -5,7 +5,7 @@ let deviceAbsolute = null;
 try
 {
     // initialising object for device orientation
-    deviceAbsolute = new AbsoluteOrientationSensor({ frequency: 10 });
+    deviceAbsolute = new AbsoluteOrientationSensor({ frequency: 30 });
 
     // if sensor is available but there is problem in using it
     deviceAbsolute.addEventListener('error', event => {
@@ -46,7 +46,9 @@ catch (error)
 // function to print value on the webpage
 let angleArray = [];
 let output = 0;
-let userHeight = 0;
+let userHeight,topAngle,baseAngle;;
+let baseAngleCheck = 0;
+let topAngleCheck = 0;
 
 function reloadOrientationValues(deviceAbsolute)
 {
@@ -59,9 +61,10 @@ function reloadOrientationValues(deviceAbsolute)
 
   angleArray.push(angle);
   output += angle;
-  if(angleArray.length == 5)
+  if(angleArray.length == 10)
   {
-    document.getElementById("bValue").innerHTML = (output/5).toFixed(2);
+    let averageAngle = (output/10).toFixed(2);
+    document.getElementById("bValue").innerHTML = averageAngle;
     angleArray = [];
     output = 0;
   }
@@ -75,12 +78,20 @@ function setCameraHeight()
 {
   let heightOfCameraRef = document.getElementById("heightOfCamera");
   userHeight = prompt("Please enter your camera height in metres.");
+  // checking whether input is valid  and keep promting
+  // until a valid input is entered
   while(isNaN(Number(userHeight)) || userHeight <= 0)
   {
     alert("Invalid input! Camera height should be a positive number.");
     userHeight = prompt("Please enter your camera height in metres.");
   }
-  heightOfCameraRef.innerHTML = userHeight;
+  heightOfCameraRef.innerHTML = userHeight + "m";
+  // enabling the calculate button only when base angle, top angle
+  // and user height are present
+  if(baseAngleCheck >= 1 && topAngleCheck >=1 && userHeight != undefined)
+  {
+    document.getElementById("calculateButton").disabled = false;
+  }
 }
 
 //Function to capture the tilt angle when user hit Top angle
@@ -89,31 +100,45 @@ function setCameraHeight()
 // --set condition where can only apply when camera is on
 function recordTopAngle()
 {
-  let bvalueRef = document.getElementById("bValue").value;
+  let bvalueRef = document.getElementById("bValue").innerText;
   let outputRef = document.getElementById("topAngle");
-
-   outputRef.innerHTML = parseFloat(bvalueRef);
+  topAngle = bvalueRef;
+  outputRef.innerHTML = topAngle + "&deg;";
+  topAngleCheck += 1;
+  // enabling the calculate button only when base angle, top angle
+  // and user height are present
+  if(baseAngleCheck >= 1 && topAngleCheck >=1 && userHeight != undefined)
+  {
+    document.getElementById("calculateButton").disabled = false;
+  }
 }
 
 function recordBaseAngle()
 {
-  let bvalueRef = document.getElementById("bValue").value;
+  let bvalueRef = document.getElementById("bValue").innerText;
   let outputRef = document.getElementById("baseAngle");
-
-  outputRef.innerHTML = bvalueRef;
+  baseAngle = bvalueRef;
+  outputRef.innerHTML = baseAngle + "&deg;";
+  baseAngleCheck += 1;
+  // enabling the calculate button only when base angle, top angle
+  // and user height are present
+  if(baseAngleCheck >= 1 && topAngleCheck >=1 && userHeight != undefined)
+  {
+    document.getElementById("calculateButton").disabled = false;
+  }
 }
 
 function calculate()
 {
   let distanceOfObjectRef = document.getElementById("distanceOfObject");
   let heightOfObjectRef = document.getElementById("heightOfObject");
-  let h1 = document.getElementById("heightOfCamera").value;
-  let beta = output*(Math.PI/180);
-  distanceOfObjectRef.innerHTML = h1 * Math.tan(beta);
+  let h1 = Number(userHeight);
+  let beta = baseAngle*(Math.PI/180);
+  let distance = h1 * Math.tan(beta);
+  distanceOfObjectRef.innerHTML = distance.toFixed(2) + "m";
 
-  let hyp = Math.sqrt(h1**2 + d**2);
-  let theta = (alpha - beta)*(Math.PI/180);
-  let phi = (180 - theta - beta)*(Math.PI/180);
-  heightOfObjectRef.innerHTML = hyp * (Math.sin(theta)/Math.sin(phi))
-
-} 
+  let alpha = (topAngle-90)*(Math.PI/180);
+  let h2 = distance * Math.sin(alpha);
+  let height = h1 + h2;
+  heightOfObjectRef.innerHTML = height.toFixed(2) + "m";
+}
