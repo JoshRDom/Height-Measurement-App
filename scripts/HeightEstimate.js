e46f6cfaeaaa7e806ace57b23b343ab46b9fa46d
Joshua Raweng <jrawengdominic@gmail.com>
2019-08-31 17:33:40 +0800

    Update HeightEstimate.js

--- scripts/HeightEstimate.js
+++ scripts/HeightEstimate.js
@@ -2,22 +2,23 @@
 
 let deviceAbsolute = null;
 // try-catch: exception handling
+let errorRef = document.getElementsById("toast");
 try
 {
     // initialising object for device orientation
-    deviceAbsolute = new AbsoluteOrientationSensor({ frequency: 50 });
+    deviceAbsolute = new AbsoluteOrientationSensor({ frequency: 60 });
 
     // if sensor is available but there is problem in using it
     deviceAbsolute.addEventListener('error', event => {
-    // Handle runtime errors.
-    if (event.error.name === 'NotAllowedError')
-    {
-      errorRef.innerText ="Permission to access sensor was denied.";
-    }
-    else if (event.error.name === 'NotReadableError' )
-    {
-      errorRef.innerText = "Cannot connect to the sensor.";
-    }});
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError')
+        {
+            errorRef.innerText ="Permission to access sensor was denied.";
+        }
+        else if (event.error.name === 'NotReadableError' )
+        {
+            errorRef.innerText = "Cannot connect to the sensor.";
+        }});
     // when sensor has a reading, call the function
     deviceAbsolute.addEventListener('reading', () => reloadOrientationValues(deviceAbsolute));
 
@@ -26,203 +27,218 @@ try
 }
 catch (error)
 {
-// Handle construction errors.
-  let errorText = "";
-  if (error.name === 'SecurityError')
-  {
-    errorText = "Sensor construction was blocked by the Feature Policy.";
-  }
-  else if (error.name === 'ReferenceError')
-  {
-    errorText = "Sensor is not supported by the User Agent.";
-  }
-  else
-  {
-    errorText = "Sensor not supported";
-  }
-  errorRef.innerText = errorText;
+    // Handle construction errors.
+    let errorText = "";
+    if (error.name === 'SecurityError')
+    {
+        errorText = "Sensor construction was blocked by the Feature Policy.";
+    }
+    else if (error.name === 'ReferenceError')
+    {
+        errorText = "Sensor is not supported by the User Agent.";
+    }
+    else
+    {
+        errorText = "Sensor not supported";
+    }
+    errorRef.innerText = errorText;
 }
 
-// function to print value on the webpage
-let betaArray = [];
-let gammaArray = [];
-let output = 0;
-let userHeight,topAngle,baseAngle,averageBeta,averageGamma;
-let baseAngleCheck = 0;
-let topAngleCheck = 0;
+// GLOBAL VARIABLES
+let instBeta; // Instantaneous beta angle values in radians
+let instGamma; // Instantaneous gamma angle values in radians
+let betaArray = []; // Array to store instantaneous beta angle values
+let userHeight; // Height of camera from the ground, estimated to be the user's own height
+let topAngle; // Beta angle of the top of the object
+let baseAngle; // Beta angle of the base of the object
 
 function reloadOrientationValues(deviceAbsolute)
 {
-  let x = deviceAbsolute.quaternion[0];
-	let y = deviceAbsolute.quaternion[1];
-	let z = deviceAbsolute.quaternion[2];
-	let w = deviceAbsolute.quaternion[3];
-	let beta = Math.atan2(2*(w*x + y*z), 1 - 2*(Math.pow(x,2)+Math.pow(y,2))); //beta
-	let gamma = Math.asin(2*(w*y - x*z)); //gamma
+    /*
+    reloadOrientationValues() is a function that
+    takes an object 'deviceAbsolute'
+    and computes the beta and gamma angle values of the device.
+    
+    beta angle values are pushed into betaArray.
+    Once the array reaches a certain length,
+    the function smoothing() will run
+    and betaArray will be emptied.
+    */
 
-  //pushing the value into the respective angle array
-  betaArray.push(beta);
-  gammaArray.push(gamma);
+    let x = deviceAbsolute.quaternion[0];
+    let y = deviceAbsolute.quaternion[1];
+    let z = deviceAbsolute.quaternion[2];
+    let w = deviceAbsolute.quaternion[3];
+    instBeta = Math.atan2(2*(w*x + y*z), 1 - 2*(Math.pow(x,2)+Math.pow(y,2)));
+    instGamma = Math.asin(2*(w*y - x*z));
 
-  //if alphaArray length is 10, betaArray and gammaArray will also be 10
-  if (betaArray.length == 10)
-  {
-    smoothing();
-    //reset all the value inside the array after every iteration
-    betaArray = [];
-    gammaArray = [];
-  }
+    betaArray.push(instBeta);
+    if (betaArray.length == 20)
+    {
+        smoothing();
+        betaArray = [];
+    }
 }
-//Function used to smooth the data
-//Output: the smoothed data from the inputted value
+
 function smoothing()
 {
-  let betaTotal = 0;
-  let gammaTotal = 0;
+    /*
+    smoothing() is a function that
+    calculates the average of angle values in betaArray
+    and prints the average into the HTML element that has the id "bValue"
+    */
+    
+    let betaTotal = 0;
 
-  for (let i in betaArray)
-  {
-    betaTotal += betaArray[i];
-    gammaTotal += gammaArray[i];
-  }
+    for (let i in betaArray)
+    {
+        betaTotal += betaArray[i];
+    }
 
-  averageBeta = ((betaTotal/10)*(180/Math.PI)).toFixed(2);
-  averageGamma =  ((gammaTotal/10)*(180/Math.PI)).toFixed(2);
-  document.getElementById("bValue").innerHTML = averageBeta;
+    let averageBeta = (betaTotal/betaArray.length)*(180/Math.PI);
+    document.getElementById("bValue").innerHTML = averageBeta.toFixed(2) + "&deg;";
 }
-// end: code for device orientation
 
-// A function to get the height
-//from the user for future calculation
-//using prompt function
-function setCameraHeight()
+function cameraHeight()
 {
-  let heightOfCameraRef = document.getElementById("heightOfCamera");
-  userHeight = prompt("Please enter your camera height in metres.");
-  // checking whether input is valid  and keep promting
-  // until a valid input is entered
-  while(isNaN(Number(userHeight)) || userHeight <= 0)
-  {
-    if(userHeight == null || userHeight == "")
+    /*
+    cameraHeight() is an onclick function that
+    prompts the user for their height
+    to be registered into the global variable 'userHeight'
+    
+    It will then check if global variables
+    'baseAngle', 'topAngle' and 'userHeight' are defined
+    so that the CALCULATE button may be enabled.
+    */
+    
+    userHeight = prompt("Please enter your camera height in metres.");
+    
+    while(isNaN(Number(userHeight)) || userHeight <= 0)
     {
-      userHeight = 1.6;
-      alert("A default height of 1.6m has been set.")
+        if(userHeight == null || userHeight == "")
+        {
+            // In the case where the input field was left empty, a default height is set.
+            userHeight = 1.6;
+            alert("A default height of 1.6m has been set.")
+        }
+        else
+        {
+            // In the case where the input is a negative number or not a number, the user will be alerted and prompted again.
+            alert("Invalid input! Camera height should be a positive number.");
+            userHeight = prompt("Please enter your camera height in metres.");
+        }
+    }
+
+    document.getElementById("heightOfCamera").innerHTML = userHeight + "m";
+    if( baseAngle != undefined && topAngle != undefined && userHeight != undefined )
+    {
+        document.getElementById("calculateButton").disabled = false;
     }
     else
     {
-      alert("Invalid input! Camera height should be a positive number.");
-      userHeight = prompt("Please enter your camera height in metres.");
+        document.getElementById("calculateButton").disabled = true;
     }
-  }
-  //end of while loop
-
-  heightOfCameraRef.innerHTML = userHeight + "m";
-  // enabling the calculate button only when base angle, top angle
-  // and user height are present
-  if(baseAngleCheck >= 1 && topAngleCheck >=1 && userHeight != undefined)
-  {
-    document.getElementById("calculateButton").disabled = false;
-  }
-  else
-  {
-    document.getElementById("calculateButton").disabled = true;
-  }
 }
 
-//Function to capture the tilt angle when user hit Top angle
-//Record the beta value when the button is clicked
-//IMPROVE:
-// --set condition where can only apply when camera is on
-function recordTopAngle()
+function measureApexAngle()
 {
-  if(averageGamma >= -30 && averageGamma <= 30 && averageBeta >= 0 && averageBeta <= 180)
-  {
-    let bvalueRef = document.getElementById("bValue").innerText;
-    let outputRef = document.getElementById("topAngle");
-    topAngle = Number(bvalueRef);
-    if(baseAngle != undefined && baseAngle > topAngle)
+    /*
+    measureApexAngle() is an onclick function that
+    checks if the device is in an appropriate orientation
+    before registering the instantaneous beta angle value
+    into the global variable 'topAngle'
+    
+    It will then check if global variables
+    'baseAngle', 'topAngle' and 'userHeight' are defined
+    so that the CALCULATE button may be enabled.
+    */
+    
+    if( instGamma >= -Math.PI/6 && instGamma <= Math.PI/6 && instBeta >= 0 && instBeta <= Math.PI )
     {
-      alert("Top angle must be greater than base angle!");
-      topAngle = undefined;
-      topAngleCheck = 0;
-      outputRef.innerHTML = "";
-    }
-    else
-    {
-      outputRef.innerHTML = topAngle + "&deg;";
-      alert("Top angle has been set successfully!");
-      topAngleCheck += 1;
-      }
+        
+        if( baseAngle != undefined && baseAngle > instBeta )
+        {
+            alert("Top angle must be greater than base angle!");
+            topAngle = undefined;
+            document.getElementById("topAngle").innerHTML = "";
+        }
+        else
+        {
+            topAngle = instBeta;
+            document.getElementById("topAngle").innerHTML = (topAngle*(180/Math.PI)).toFixed(2) + "&deg;";
+            alert("Top angle has been set successfully!");
+        }
 
-    // enabling the calculate button only when base angle, top angle
-    // and user height are present
-    if(baseAngleCheck >= 1 && topAngleCheck >=1 && userHeight != undefined)
-    {
-      document.getElementById("calculateButton").disabled = false;
+        if( baseAngle != undefined && topAngle != undefined && userHeight != undefined )
+        {
+            document.getElementById("calculateButton").disabled = false;
+        }
+        else
+        {
+            document.getElementById("calculateButton").disabled = true;
+        }
     }
     else
     {
-      document.getElementById("calculateButton").disabled = true;
+        alert("Device orientation is invalid!");
     }
-  }
-  else
-  {
-    alert("Device orientation is invalid!");
-  }
 }
 
-function recordBaseAngle()
+function measureBaseAngle()
 {
-  if(averageGamma >= -30 && averageGamma <= 30 && averageBeta >= 0 && averageBeta <= 180)
-  {
-    let bvalueRef = document.getElementById("bValue").innerText;
-    let outputRef = document.getElementById("baseAngle");
-    baseAngle = Number(bvalueRef);
-    if(topAngle != undefined && topAngle < baseAngle)
+    /*
+    measureBaseAngle() is an onclick function that
+    checks if the device is in an appropriate orientation
+    before registering the instantaneous beta angle value
+    into the global variable 'baseAngle'
+    
+    It will then check if global variables
+    'baseAngle', 'topAngle' and 'userHeight' are defined
+    so that the CALCULATE button may be enabled.
+    */
+    
+    if( instGamma >= -Math.PI/6 && instGamma <= Math.PI/6 && instBeta >= 0 && instBeta <= Math.PI )
     {
-      alert("Base angle must be smaller than top angle!");
-      baseAngle = undefined;
-      baseAngleCheck = 0;
-      outputRef.innerHTML = "";
-    }
-    else
-    {
-      outputRef.innerHTML = baseAngle + "&deg;";
-      alert("Base angle has been set successfully!");
-      baseAngleCheck += 1;
-    }
+        
+        if(topAngle != undefined && topAngle < instBeta)
+        {
+            alert("Base angle must be smaller than top angle!");
+            baseAngle = undefined;
+            document.getElementById("baseAngle").innerHTML = "";
+        }
+        else
+        {
+            baseAngle = instBeta;
+            document.getElementById("baseAngle").innerHTML = (baseAngle*(180/Math.PI)).toFixed(2) + "&deg;";
+            alert("Base angle has been set successfully!");
+        }
 
-    // enabling the calculate button only when base angle, top angle
-    // and user height are present
-    if(baseAngleCheck >= 1 && topAngleCheck >=1 && userHeight != undefined)
-    {
-      document.getElementById("calculateButton").disabled = false;
+        if( baseAngle != undefined && topAngle != undefined && userHeight != undefined )
+        {
+            document.getElementById("calculateButton").disabled = false;
+        }
+        else
+        {
+            document.getElementById("calculateButton").disabled = true;
+        }
     }
     else
     {
-      document.getElementById("calculateButton").disabled = true;
+        alert("Device orientation is invalid!");
     }
-  }
-  else
-  {
-    alert("Device orientation is invalid!");
-  }
 }
 
-// Function to calculate the distance and height
-// Output: return height of the object
-//       : distance from user to the object
 function calculate()
 {
-  let distanceOfObjectRef = document.getElementById("distanceOfObject");
-  let heightOfObjectRef = document.getElementById("heightOfObject");
-  let h1 = Number(userHeight);
-  let beta = baseAngle*(Math.PI/180);
-  let distance = h1 * Math.tan(beta);
-  distanceOfObjectRef.innerHTML = distance.toFixed(2) + "m";
+    /*
+    calculate() is an onclick function that
+    takes global variables 'userHeight', 'baseAngle' and 'topAngle'
+    to calculate the distance the object is from the user, and the object's height.
+    */
+    
+    let distance = Number(userHeight) * Math.tan(baseAngle);
+    document.getElementById("distanceOfObject").innerHTML = distance.toFixed(2) + "m";
 
-  let alpha = (topAngle-90)*(Math.PI/180);
-  let h2 = distance * Math.sin(alpha);
-  let height = h1 + h2;
-  heightOfObjectRef.innerHTML = height.toFixed(2) + "m";
+    let height = Number(userHeight) + distance * Math.tan(topAngle - Math.PI/2);
+    document.getElementById("heightOfObject").innerHTML = height.toFixed(2) + "m";
 }
